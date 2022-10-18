4c6be9eca27e546dfd4d256ce8e9110344ff54d1
Syrine-B <113478812+Syrine-B@users.noreply.github.com>
2022-10-18 18:15:35 +0200

    Add files via upload

---
+++ code_donneur_max.py
@@ -0,0 +1,159 @@
+#!/usr/bin/python3
+###############
+########### Utilisation
+########### Python3 code.py [uts complet] [arbre complet] [arbre prune] [numero de gene]
+###########
+###############
+
+from ete3 import Tree
+import argparse
+import sys
+
+arbre_zombi= Tree(sys.argv[2],format=1) #grand
+arbre_vivant = Tree(sys.argv[3],format =1) #petit
+
+liste = []
+
+liste_feuille_vivante = [i.name for i in arbre_vivant]
+liste_feuille = [i.name for i in arbre_zombi]
+liste_morte = list(set(liste_feuille)-set(liste_feuille_vivante))
+
+ale = open(sys.argv[1],"r")
+
+transfert = ale.readlines()
+
+transfert_donneur = []
+transfert_receveur = []
+score=[]
+
+num_gene = sys.argv[4]
+
+nouveau_transfert = open("new_transfert_donneur.txt","a")
+
+try: 
+    info = transfert[1].split()
+
+except:
+	nouveau_transfert.write("null")
+	exit()
+
+del transfert[0]
+for line in transfert:
+	events = line.split()
+	donneur_a = events[0].split("(")
+	receveur_a = events[1].split("(")
+	transfert_donneur.append(donneur_a[0])
+	transfert_receveur.append(receveur_a[0])
+	score.append(events[2])
+
+
+
+#########PARTIE 2 : transformer ces deux listes (donneur receveur) en donneur-receveur dans l'arbre réduit
+new_transfert_donneur = []
+
+for d in transfert_donneur:
+	node = arbre_zombi&d
+	leaves = [i.name for i in node]
+	descornot = len(list(set(liste_feuille_vivante) & set(leaves)))
+	while (descornot==0 and node.is_root()!=True):
+		node=node.up
+		leaves = [i.name for i in node]
+		descornot = len(list(set(liste_feuille_vivante) & set(leaves)))
+	if (descornot==1):
+		new_transfert_donneur.append(list(set(liste_feuille_vivante) & set(leaves))[0])
+	else:
+		new_transfert_donneur.append(arbre_vivant.get_common_ancestor(list(set(liste_feuille_vivante) & set(leaves))).name)
+
+new_transfert_receveur = []
+
+for d in transfert_receveur:
+	node = arbre_zombi&d
+	leaves = [i.name for i in node]
+	descornot = len(list(set(liste_feuille_vivante) & set(leaves)))
+	if (descornot==0):
+		new_transfert_receveur.append("none")
+	elif (descornot==1):
+		new_transfert_receveur.append(list(set(liste_feuille_vivante) & set(leaves))[0])
+	else:
+		new_transfert_receveur.append(arbre_vivant.get_common_ancestor(list(set(liste_feuille_vivante) & set(leaves))).name)
+
+transfert_new = []
+
+transfert_disparu = []
+
+for k in range(len(new_transfert_donneur)):
+	if (new_transfert_receveur[k]!="none"):
+		transfert_new.append(new_transfert_donneur[k] + " - " + new_transfert_receveur[k]+ " - "+ score[k])
+	elif(new_transfert_receveur[k]=="none"):
+		transfert_disparu.append( transfert_donneur[k] + ";" + "null" + ";" + str(score[k]) + ";" + "0")
+
+
+mes_transferts = []
+
+for i in range(len(transfert_new)):
+	for j in range(len(transfert_new)):
+		if(i!=j):
+			transfert_1 = transfert_new[i].split("-")
+			transfert_2 = transfert_new[j].split("-")
+
+			if (str(transfert_1[0]) == str(transfert_2[0]) and str(transfert_1[1])==str(transfert_2[1])):
+				if (float(transfert_1[2])>float(transfert_2[2])):
+					mes_transferts.append(transfert_new[i])
+					liste.append(j)
+					liste.append(i)
+				elif(float(transfert_1[2])<float(transfert_2[2])):
+					mes_transferts.append(transfert_new[j])
+					liste.append(j)
+					liste.append(i)
+
+
+
+for i in range(len(transfert_new)):
+	if i not in liste:
+		mes_transferts.append(transfert_new[i])
+		liste.append(i)
+
+nouveau_transfert = open("new_transfert_donneur.txt","a")
+
+
+transfert_new_final = list(set(mes_transferts))
+
+for i in range(len(transfert_new_final)):
+	nouveau_transfert.write(str(transfert_new_final[i])+"\n")
+
+sortie =  open("sortie_donneur.txt", "a") 
+
+
+for i in range(len(transfert_disparu)):
+	info = tansfert_disparu[i].split(";")
+	disp.append(info[0])
+	disp_score.append(info[2])
+
+
+
+a_suppr=[]
+
+for i in range(len(disp)):
+    for j in range(len(disp)):
+        if i!=j and j>i and disp[i] == disp[j] and float(disp_score[i]) < float(disp_score[j]) :
+          disp_score[i] = float(disp_score[j])
+		  a_suppr.append(j)
+
+
+
+a_suppr=set(a_suppr)
+a_suppr = sorted(a_suppr)
+
+a_suppr.reverse()
+
+for i in range(len(a_suppr)):
+    index = a_suppr[i]
+    del disp[index]
+    del disp_score[index]
+
+for i in range(len(disp)):
+	transfert_disparu.append(disp[i] + ";" + "null" + ";" + str(disp_score[i]) + ";" + "0")
+
+
+for i in range(len(transfert_disparu)):
+	sortie.write(num_gene+ ";" +str(transfert_disparu[i])+"\n")
---
+++ sortie_donneur.py
@@ -0,0 +1,168 @@
+#!/usr/bin/python3
+###############
+########### Utilisation
+########### Python3 sortie.py [uts prune] [numero de gene]
+########### a besoin pour fonctionner du fichier new_transfert obtenua l'aide de code.py
+###############
+################## Importation ##################
+
+from ete3 import Tree
+import argparse
+import sys
+
+
+################## declaration ##################
+score_1 = []
+score_2 = []
+
+transfert_ale_1 = []
+transfert_ale_2 = []
+resume = []
+
+num_gene = sys.argv[2]
+
+sortie =  open("sortie_donneur.txt", "a") 
+
+################## Fonction ##################
+
+def recherche_inverse(dico, valeur):
+    for k in dico:
+        if dico[k] == valeur:
+            return k
+    raise LookupError()
+
+
+###############################################################################################
+
+
+ale_1 = open("new_transfert_donneur.txt","r")
+ale_2 = open(sys.argv[1],"r")
+
+################## isolement des espèces donneuses et receveuses ##################
+ligne_ale_1 = ale_1.readlines()
+lignes_ale_2 = ale_2.readlines()
+try:
+    info = ligne_ale_1[1].split(" ")
+
+except:
+    
+    try:
+        info = lignes_ale_2[1].split()
+    
+    except:
+        sortie.write(num_gene+";"+"null"+";"+"null"+";"+"0"+";"+"0"+"\n")
+        exit()
+    
+    del lignes_ale_2[0]
+    for i in lignes_ale_2:
+        info = i.split()
+        donneur_a = info[0].split("(")
+        receveur_a = info[1].split("(")
+        transfert_ale_2.append(donneur_a[0])
+        score = info[2].split()
+        score_2.append(score[0])
+    
+    for j in range(len(transfert_ale_2)):
+        sortie.write(num_gene+";"+"null"+";"+str(transfert_ale_2[j])+";"+"0"+";"+str(score_2[j])+"\n")
+    
+    exit()
+
+for i in ligne_ale_1:
+    info = i.split(" ")
+    transfert_ale_1.append(info[0])
+    score = info[4].split()
+    score_1.append(score[0])
+       
+################## lecture du fichier transfert de ALE ##################
+
+
+
+try: 
+    info = lignes_ale_2[1].split()
+
+except: 
+    for i in range(len(transfert_ale_1)):
+        sortie.write(num_gene+";"+str(transfert_ale_1[i])+";"+"null"+";"+str(score_1[i])+";"+"0"+"\n")
+    exit()
+
+################## Si fichier transfert Ale non vide ##################
+del lignes_ale_2[0]
+for i in lignes_ale_2:
+    info = i.split()
+    donneur_a = info[0].split("(")
+    receveur_a = info[1].split("(")
+    transfert_ale_2.append(donneur_a[0])
+    score = info[2].split()
+    score_2.append(score[0])
+        
+################## vérification correspondance transfert ##################
+
+for i in range(len(transfert_ale_1)):
+    for j in range(len(transfert_ale_1)):
+        if i!=j and j>i:
+            if transfert_ale_1[i] == transfert_ale_1[j]:
+                score_1[i] = float(score_1[i])+float(score_1[j])
+
+
+for i in range(len(transfert_ale_2)):
+    for j in range(len(transfert_ale_2)):
+        if i!=j and j>i:
+            if transfert_ale_2[i] == transfert_ale_2[j]:
+                score_2[i] = float(score_2[i])+float(score_2[j])
+
+
+a_suppr=[]
+
+for i in range(len(transfert_ale_1)):
+    for j in range(len(transfert_ale_1)):
+        if i!=j and i<j:
+            if transfert_ale_1[i] == transfert_ale_1[j]:
+                a_suppr.append(j)
+
+a_suppr=set(a_suppr)
+a_suppr = sorted(a_suppr)
+
+a_suppr.reverse()
+
+for i in range(len(a_suppr)):
+    index = a_suppr[i]
+    del transfert_ale_1[index]
+    del score_1[index]
+
+a_suppr_2 =[]
+
+for i in range(len(transfert_ale_2)):
+    for j in range(len(transfert_ale_2)):
+        if i!=j and i<j:
+            if transfert_ale_2[i] == transfert_ale_2[j]:
+                a_suppr_2.append(j)
+
+a_suppr_2=set(a_suppr_2)
+a_suppr_2 = sorted(a_suppr_2)
+
+a_suppr_2.reverse()
+
+for i in range(len(a_suppr_2)):
+    index = a_suppr_2[i]
+    del transfert_ale_2[index]
+    del score_2[index]
+
+
+
+
+for i in range(len(transfert_ale_1)):
+    for j in range(len(transfert_ale_2)):
+        if transfert_ale_1[i]==transfert_ale_2[j]:
+            resume.append(transfert_ale_1[i] + ";" + transfert_ale_2[j]+";"+str(score_1[i])+";"+str(score_2[j]))
+
+for i in range(len(transfert_ale_1)):
+    if transfert_ale_1[i] not in transfert_ale_2:
+        resume.append(transfert_ale_1[i]+";"+"null"+";"+str(score_1[i])+";"+"0")
+
+for i in range(len(transfert_ale_2)):
+    if transfert_ale_2[i] not in transfert_ale_1:
+        resume.append("null"+";"+transfert_ale_2[i]+";"+"0"+";"+str(score_2[i]))
+
+
+for i in range(len(resume)):
+	sortie.write(num_gene+";"+str(resume[i])+"\n")
